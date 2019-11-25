60da70d03bdc80c7e444fc1697ec8e1c30c9009d
JoLeeSoftwire <Jo.Lee@softwire.com>
2019-11-25 11:24:22 +0000

    Dependencies

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,13 @@
 package training.busboard;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+
 public class Main {
     public static void main(String args[]) {
         // Your code here!
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
     }
 }	
