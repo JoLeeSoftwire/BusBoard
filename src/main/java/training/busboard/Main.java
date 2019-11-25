1e02540f18d580f266ab9e2e0e9bb943faf6d9cb
Dulcie Jackson <Dulcie.Jackson@softwire.com>
2019-11-25 12:12:07 +0000

    Added web setup and simple GET request

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -2,12 +2,30 @@ package training.busboard;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
-        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+    public static void main(String args[]) throws Exception {
+        Caller caller = new Caller();
+        caller.setUp();
+        caller.getArrivals("490008660N");
     }
-}	
+}
+
+class Caller {
+    private String BASE_URI = "https://api.tfl.gov.uk";
+    private WebTarget target;
+
+    public void setUp() throws Exception {
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        target = client.target(BASE_URI);
+    }
+
+    public void getArrivals(String stopID) {
+        String responseMsg = target.path("/StopPoint/" + stopID + "/Arrivals").request().get(String.class);
+        System.out.println(responseMsg);
+    }
+}
